dfebc1c1f30738981918066cbf7b5d5108edd627
shinjuwu <shinjuwu0830@gmail.com>
2018-06-05 17:29:32 +0800

    html pattern

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"DTmocker/controller"
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/api/", controller.ShowApiList)
+	http.HandleFunc("/login", login)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		log.Println(t.Execute(w, nil))
+	} else {
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
 }
